5be9b647f29fa7b80dbd6c159fecb495354d997e
safri <safrisyamsudin123@gmail.com>
2021-08-25 00:08:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import moment from 'moment'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            'isLoading': false,
+            'barisData': []
+        }
+    }
+
+    componentDidMount() {
+        document.title = 'Render Data using state and lifecycle'
+        this.getRows()
+    }
+
+    async getRows() {
+        this.setState({
+            'isLoading': true
+        })
+        let urlAPI = `https://swapi.dev/api/planets/?format=json`
+        let res = await axios.get(urlAPI).then(res => {
+            return res.data.results
+        }).catch(err => {
+            console.log(err)
+        })
+
+        this.setState((state, props) => {
+            return {
+                barisData: state.barisData.concat(res),
+                isLoading: false
+            }
+        })
+    }
+
+    render() {
+
+        const renderConditionalContent = () => {
+            if (this.state.isLoading) {
+                return <tr><td colSpan="6" align="center"><b>Loading....</b></td></tr>
+            } else {
+                return this.state.barisData.map((dat, index) => {
+                    return (
+                        <tr key={index}>
+                            <td>{index + 1}.</td>
+                            <td>{dat.name}</td>
+                            <td>{dat.diameter}</td>
+                            <td>{dat.population}</td>
+                            <td>{dat.gravity}</td>
+                            <td>{moment(dat.created).format("DD MMM YYYY")}</td>
+                        </tr>
+                    )
+                })
+            }
+        }
+
+        return (
+            <div>
+                <h1 align="center">Render Data using state and lifecycle</h1>
+                <table cellPadding="15" border="1px solid black" cellSpacing="0" style={{ marginLeft: 'auto', marginRight: 'auto', width: '60%' }}>
+                    <thead>
+                        <tr style={{ backgroundColor: 'orange' }}>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>Diameter</th>
+                            <th>Population</th>
+                            <th>Gravity</th>
+                            <th>Created</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {renderConditionalContent()}
+                    </tbody>
+                </table>
+
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
